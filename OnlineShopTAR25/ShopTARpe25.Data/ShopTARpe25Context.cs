09be56429c9e4d57e6506b9331d357ac31cb46f3
Opilane Techno+TLN <opilane@tthk.ee>
2026-09-03 13:41:05 +0300

    new project
    added a database connection

---
+++ OnlineShopTAR25/ShopTARpe25.Data/ShopTARpe25Context.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShopTARpe25.Data
+{
+    // teha sellest classist DbContext, et saaks andmebaasi kasutada
+    public class ShopTARpe25Context : DbContext
+    {
+        public ShopTARpe25Context(DbContextOptions<ShopTARpe25Context> options) : base (options)
+        {
+
+        }
+    }
+}
